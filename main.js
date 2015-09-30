2f2bc9cb3d67b96570170368067fd15d29d0c54d
mulian <Max.Hoffmann@tu-bs.de>
2015-09-30 20:52:27 +0200

    Coffee-script is finnaly added

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+// var app = require('app');  // Module to control application life.
+// var BrowserWindow = require('browser-window');  // Module to create native browser window.
 var fs = require('fs')
 // var packageFile = require('./package.json');
 
@@ -8,30 +8,20 @@ var coffee = require('coffee-script');
 require('crash-reporter').start();
 var coffeePath='./lib/', jsPath='./src/'
 
-var numFiles=0;
-
 compileToCoffe = function(file_path,file_name) {
   // console.log("compileToCoffe: "+file_path+" name: "+file_name);
   var file_name_js = file_name.slice(0,file_name.length-7)+'.js';
   // console.log(file_name_js);
-  fs.readFile(file_path,'utf8',function(err,data) {
-    // console.log(data);
-    fs.writeFile(jsPath+file_name_js,coffee.compile(data),function(err) {
-      if(err) throw err;
-      console.log("Compile: "+file_path+" to "+jsPath+file_name_js);
-      numFiles--;
-      //run main.js (aka. main.coffee), after compile
-      if(numFiles==0) require(jsPath+'main.js')(app,BrowserWindow)
-    });
-  });
+  var data=fs.readFileSync(file_path,'utf8');
+  fs.writeFileSync(jsPath+file_name_js,coffee.compile(data));
 };
 
-fs.readdir(coffeePath,function(err,files) {
-  if(err) throw err;
-  numFiles=files.length;
-  for(var i=0;i<files.length;i++) {
-    var file = files[i];
-    // console.log("compile: "+file);
-    compileToCoffe(coffeePath+file,file);
-  }
-});
+files = fs.readdirSync(coffeePath);
+for(var i=0;i<files.length;i++) {
+  var file = files[i];
+  // console.log("compile: "+file);
+  compileToCoffe(coffeePath+file,file);
+}
+
+//Start src/main.js (lib/main.coffee)
+require(jsPath+'main.js')
