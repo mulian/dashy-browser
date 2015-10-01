66f50ec2b803950b4c7e333e4502b859b68d3a46
Max <maxh@MaxBook.local>
2015-10-01 08:47:07 +0200

    deploy less support

--- main.js
+++ main.js
@@ -3,14 +3,16 @@
 var fs = require('fs')
 // var packageFile = require('./package.json');
 
+
+//COFFEESCRIPT
 var coffee = require('coffee-script');
 // console.log(coffee);
-require('crash-reporter').start();
+
 var coffeePath='./lib/', jsPath='./src/'
 
 compileToCoffe = function(file_path,file_name) {
   // console.log("compileToCoffe: "+file_path+" name: "+file_name);
-  var file_name_js = file_name.slice(0,file_name.length-7)+'.js';
+  var file_name_js = file_name.slice(0,file_name.length-6)+'js';
   // console.log(file_name_js);
   var data=fs.readFileSync(file_path,'utf8');
   fs.writeFileSync(jsPath+file_name_js,coffee.compile(data));
@@ -22,6 +24,23 @@ for(var i=0;i<files.length;i++) {
   // console.log("compile: "+file);
   compileToCoffe(coffeePath+file,file);
 }
+
+//LESS
+var lessPath='./less/', cssPath='./css/'
+var less = require('less');
+files = fs.readdirSync(lessPath);
+for(var i=0;i<files.length;i++) {
+  var file = files[i];
+  var cssFile = file.slice(0,file.length-4)+'css';
+  // console.log("compile: "+file);
+  var fileDate = fs.readFileSync(lessPath+file,'utf8');
+  // console.log(fileDate);
+  less.render(fileDate,function (e,output) {
+    //console.log(output); //maby use output.imports?
+    fs.writeFileSync(cssPath+cssFile,output.css,'utf8');
+  });
+}
+
 
 //Start src/main.js (lib/main.coffee)
 require(jsPath+'main.js')
